50eddb3b776163092883101099badf5f107b0ba6
rolealiu <rolealiu@rippletec.net>
2017-11-07 10:32:12 +0800

    v1.0

---
+++ config.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/env python
+project_key = ''
+secret_key = ''
+file_path = 'C:\\project\\code_upload'
+file_suffix = 'java'
+exclude_file = ['gitignore','config.php']
+user_name = '' #eolinker账号
+user_password = '' #密码
+
